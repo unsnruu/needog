d7af067ec86379a88c37c9f0a936bc3c0319ba44
unsnruu <unsnruu@gmail.com>
2022-04-01 13:46:35 +0900

    Make initial route logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+import { Home } from "./Pages/Home";
+import { NotFound } from "./Pages/404";
+import { Adoption } from "./Pages/Adoption";
+import { Missing } from "./Pages/Missing";
+import { Care } from "./Pages/Care";
+import { Community } from "./Pages/Community";
+import { SignIn } from "./Pages/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="adoption" element={<Adoption />}></Route>
+        <Route path="missing" element={<Missing />}></Route>
+        <Route path="care" element={<Care />}></Route>
+        <Route path="community" element={<Community />}></Route>
+        <Route path="signin" element={<SignIn />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
