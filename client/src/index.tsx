51e9cb47b65608cd1337878fe40d5311d511e5a3
unsnruu <unsnruu@gmail.com>
2022-04-05 00:25:44 +0900

    Add RecoilRoot comp

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import axios from "axios";
+
 import App from "./App";
+
+axios.defaults.baseURL = "http://localhost:8000";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
