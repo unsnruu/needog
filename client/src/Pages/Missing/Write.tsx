c5203d8149fdbdb431a33708e367c3c89ff2e43f
unsnruu <unsnruu@gmail.com>
2022-04-20 22:40:43 +0900

    Record Error for self-development

--- client/src/Pages/Missing/Write.tsx
+++ client/src/Pages/Missing/Write.tsx
@@ -1,99 +1,61 @@
-import React, { useState } from "react";
+//Todo : 로그인 시에만 이 페이지 방문 가능하도록 navigate해야한다.
+import React, { ChangeEvent, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { WriteForm } from "../../components/Form";
 
-type ChangeEventElement =
-  | HTMLTextAreaElement
-  | HTMLInputElement
-  | HTMLSelectElement;
+export interface Docs {
+  petKind: PetKind;
+  sido: string;
+  sigungu: string;
+  title: string;
+  text: string;
+}
+
+type PetKind = "dog" | "cat" | "misc" | "*";
 
 function Write() {
-  const [doc, setDoc] = useState({
+  const navigate = useNavigate();
+  const [docs, setDocs] = useState<Docs>({
     title: "",
     text: "",
-    pet: "all",
-    location: "all",
-    region: null,
+    petKind: "*",
+    sido: "*",
+    sigungu: "*",
   });
-  const navigate = useNavigate();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    try {
-      axios.post("/write", doc, { withCredentials: true });
-      navigate("/missing");
-    } catch (err) {
-      console.log("포스트를 생성하는데 실패하였습니다.");
-    }
+    console.log(docs);
+    // try {
+    //   axios.post("/write", docs, { withCredentials: true });
+    //   navigate("/missing");
+    // } catch (err) {
+    //   console.log("포스트를 생성하는데 실패하였습니다.");
+    // }
   };
-  const handleChange = ({ target }: React.ChangeEvent<ChangeEventElement>) => {
-    setDoc((prev) => ({ ...prev, [target.id]: target.value }));
-    console.log(doc);
+  const handleChangeTitle = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setDocs((prev) => ({ ...prev, title: target.value }));
+  };
+  const handleChageText = ({ target }: ChangeEvent<HTMLTextAreaElement>) => {
+    setDocs((prev) => ({ ...prev, text: target.value }));
   };
 
   return (
-    <form
-      style={{ display: "flex", flexDirection: "column" }}
-      onSubmit={handleSubmit}
-    >
-      <div>
-        <label htmlFor="title">제목</label>
-        <input
-          type="text"
-          id="title"
-          onChange={handleChange}
-          value={doc.title}
-        />
-      </div>
-      <div>
-        <label htmlFor="pet">종류</label>
-        <select name="pets" id="pet" onChange={handleChange} value={doc.pet}>
-          <option value="all" disabled>
-            반려동물의 종류를 선택하세요
-          </option>
-          <option value="dog">강아지</option>
-          <option value="cat">고양이</option>
-          <option value="misc">기타</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="location">지역</label>
-        <select
-          name="locations"
-          id="location"
-          onChange={handleChange}
-          value={doc.location}
-        >
-          <option value="all" disabled>
-            지역을 선택하세요
-          </option>
-          <option value="seoul">서울</option>
-          <option value="incheon">인천</option>
-          <option value="gyeonggi">경기</option>
-          <option value="busan">부산</option>
-          <option value="daegeon">대전</option>
-          <option value="gwangju">광주</option>
-        </select>
-      </div>
-      {doc.location === "all" ? null : (
-        <div>
-          <label htmlFor="region">상세 지역</label>
-          <select name="regions" id="region" onChange={handleChange}>
-            <option value=""></option>
-          </select>
-        </div>
-      )}
-      <br />
-      <textarea
-        id="text"
-        cols={30}
-        rows={10}
-        style={{ outline: "none", resize: "none" }}
-        onChange={handleChange}
-        value={doc.text}
-      ></textarea>
-      <button>게시글 등록하기</button>
-    </form>
+    <div>
+      <label htmlFor="title">제목</label>
+      <input type="text" id="title" onChange={handleChangeTitle} />
+      <WriteForm
+        selected={{
+          petKind: docs.petKind,
+          sido: docs.sido,
+          sigungu: docs.sigungu,
+        }}
+        setSelected={setDocs}
+      ></WriteForm>
+      <textarea style={{ resize: "none" }} onChange={handleChageText} />
+      <button>글쓰기</button>
+    </div>
   );
 }
 
--- client/src/components/Form/WriteForm.tsx
+++ client/src/components/Form/WriteForm.tsx
@@ -1,9 +1,18 @@
-//Todo : 아무것도 수정안함 수정 필요함
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+//Todo : title과 text를 다루는 state가 필요로 함.
+//Todo : useEffect부분은 어차피 겹치는 거 그냥 custom hook으로 빼면 좋을 듯.
+//todo:  handleChange도 그냥 전달 받는 게 나을지도~
+// * SearchForm과는 다른 점(1): handleSubmit을 상위에서 전달받는다.
+
+import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
 
 import Select, { OptionItem } from "../Select";
+import { getRegSidos, getRegSidoguns } from "../../api";
+import { Docs } from "../../Pages/Missing/Write";
 
+interface WriteFormProps {
+  setSelected: Dispatch<SetStateAction<Docs>>;
+  selected: Selected;
+}
 interface Selected {
   petKind: PetKind;
   sido: string;
@@ -19,14 +28,6 @@ interface PetOption extends OptionItem {
 }
 type PetKind = "dog" | "cat" | "misc" | "*";
 
-interface RegCode {
-  name: string;
-  code: string;
-}
-interface ReturnAxios {
-  regcodes: RegCode[];
-}
-
 const petKindsInit: PetOption[] = [
   { key: "*", text: "모든 동물" },
   { key: "dog", text: "강아지" },
@@ -34,13 +35,7 @@ const petKindsInit: PetOption[] = [
   { key: "misc", text: "기타" },
 ];
 
-function WriteForm() {
-  const [selected, setSelected] = useState<Selected>({
-    petKind: "*",
-    sido: "*",
-    sigungu: "*",
-  });
-
+function WriteForm({ selected, setSelected }: WriteFormProps) {
   const [selects, setSelects] = useState<Selects>({
     petKinds: petKindsInit,
     sidos: [],
@@ -51,11 +46,7 @@ function WriteForm() {
     async function getAllSidos() {
       if (selects.sidos.length) return;
 
-      const {
-        data: { regcodes },
-      } = await axios.get<ReturnAxios>(
-        "https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=*00000000"
-      );
+      const regcodes = await getRegSidos();
       const allSidos: OptionItem[] = regcodes.map(({ code, name }) => ({
         key: code.slice(0, 2),
         text: name,
@@ -71,11 +62,7 @@ function WriteForm() {
         return;
       }
 
-      const {
-        data: { regcodes },
-      } = await axios.get<ReturnAxios>(
-        `https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=${sidoCode}*00000&is_ignore_zero=true`
-      );
+      const regcodes = await getRegSidoguns(sidoCode);
 
       const newSidoguns: OptionItem[] = regcodes.map(({ code, name }) => ({
         key: code.slice(2, 5),
@@ -87,21 +74,17 @@ function WriteForm() {
 
     getAllSidos();
     getSidoguns(selected.sido);
-  }, [selected.sido, selects.sidos]);
+    console.log("sigungus:", selects.sigungus);
+  }, [selected.sido, selects.sidos, selects.sigungus]);
 
-  const createHandleChange = (selecetedKey: keyof Selected) => {
+  const createHandleChange = (key: keyof Selected) => {
     return ({ target }: React.ChangeEvent<HTMLSelectElement>) => {
-      setSelected((prev) => ({ ...prev, [selecetedKey]: target.value }));
+      setSelected((prev) => ({ ...prev, [key]: target.value }));
     };
-  };
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("handleSubmit executed");
-    console.log(selected);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <Select
         optionItems={selects.petKinds}
         handleChange={createHandleChange("petKind")}
@@ -116,8 +99,6 @@ function WriteForm() {
           handleChange={createHandleChange("sigungu")}
         />
       ) : null}
-
-      <button>검색</button>
     </form>
   );
 }
