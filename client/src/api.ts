a64ebf820a5e96a71103ce60ae5e6a036664c1e8
unsnruu <unsnruu@gmail.com>
2022-04-21 12:59:06 +0900

    Create cumstom hook to use api easily

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,5 +1,8 @@
 //API docs: https://juso.dev/
 import axios from "axios";
+import { kill } from "process";
+import { useState, useEffect } from "react";
+import { OptionItem } from "./components/Select";
 
 interface ReturnAxios {
   regcodes: RegCode[];
@@ -8,9 +11,15 @@ interface RegCode {
   name: string;
   code: string;
 }
-interface JusoUrlParams {
-  query: string;
+interface Sido {
+  selected: string;
+  items: OptionItem[];
 }
+interface Sigungu {
+  selected: string;
+  items: OptionItem[];
+}
+
 const jusoUrl = `https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=`;
 
 async function getRegSidos() {
@@ -29,4 +38,53 @@ async function getRegSidoguns(sidoCode: string) {
   return regcodes;
 }
 
-export { getRegSidos, getRegSidoguns };
+function useRegion() {
+  const [sidos, setSidos] = useState<Sido>({
+    selected: "*",
+    items: [],
+  });
+  const [sigungus, setSigungus] = useState<Sigungu>({
+    selected: "*",
+    items: [],
+  });
+
+  useEffect(() => {
+    async function getAllSidos() {
+      if (sidos.items.length) return;
+
+      const regcodes = await getRegSidos();
+      const allSidos: OptionItem[] = regcodes.map(({ code, name }) => ({
+        key: code.slice(0, 2),
+        text: name,
+      }));
+      allSidos.unshift({ key: "*", text: "모든 지역" });
+
+      setSidos((prev) => ({ ...prev, items: allSidos }));
+    }
+
+    getAllSidos();
+  }, [sidos.items]);
+
+  useEffect(() => {
+    async function getSidoguns(sidoCode: string) {
+      if (sidoCode === "*") {
+        setSigungus((prev) => ({ ...prev, items: [] }));
+        return;
+      }
+
+      const regcodes = await getRegSidoguns(sidoCode);
+
+      const newSidoguns: OptionItem[] = regcodes.map(({ code, name }) => ({
+        key: code.slice(2, 5),
+        text: name.split(" ").splice(1).join(" "),
+      }));
+      newSidoguns.unshift({ key: "*", text: "모든 지역" });
+      setSigungus((prev) => ({ ...prev, items: newSidoguns }));
+    }
+    getSidoguns(sidos.selected);
+  }, [sidos.selected]);
+
+  return { sidos, setSidos, sigungus, setSigungus };
+}
+
+export { useRegion };
