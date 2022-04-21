de17f202096d30b894f06da4c40b0aa2264c4f92
unsnruu <unsnruu@gmail.com>
2022-04-21 13:22:02 +0900

    Remove data fetch logic and create handler func

--- client/src/components/Form/WriteForm.tsx
+++ client/src/components/Form/WriteForm.tsx
@@ -6,27 +6,20 @@
 import React, { Dispatch, SetStateAction, useState, useEffect } from "react";
 
 import Select, { OptionItem } from "../Select";
-import { getRegSidos, getRegSidoguns } from "../../api";
+import { useRegion } from "../../api";
 import { Docs } from "../../Pages/Missing/Write";
 
 interface WriteFormProps {
   setSelected: Dispatch<SetStateAction<Docs>>;
-  selected: Selected;
 }
-interface Selected {
-  petKind: PetKind;
-  sido: string;
-  sigungu: string;
-}
-interface Selects {
-  petKinds: PetOption[];
-  sidos: OptionItem[];
-  sigungus: OptionItem[];
+interface PetState {
+  selected: PetKind;
+  items: PetOption[];
 }
 interface PetOption extends OptionItem {
   key: PetKind;
 }
-type PetKind = "dog" | "cat" | "misc" | "*";
+type PetKind = "dog" | "cat" | "misc" | "*" | string;
 
 const petKindsInit: PetOption[] = [
   { key: "*", text: "모든 동물" },
@@ -34,71 +27,47 @@ const petKindsInit: PetOption[] = [
   { key: "cat", text: "고양이" },
   { key: "misc", text: "기타" },
 ];
+type HandleChangeSelect = React.ChangeEvent<HTMLSelectElement>;
 
-function WriteForm({ selected, setSelected }: WriteFormProps) {
-  const [selects, setSelects] = useState<Selects>({
-    petKinds: petKindsInit,
-    sidos: [],
-    sigungus: [],
+function WriteForm({ setSelected }: WriteFormProps) {
+  const [pet, setPets] = useState<PetState>({
+    selected: "*",
+    items: petKindsInit,
   });
+  const { sido, setSido, sigungu, setSigungu } = useRegion();
 
-  useEffect(() => {
-    async function getAllSidos() {
-      if (selects.sidos.length) return;
-
-      const regcodes = await getRegSidos();
-      const allSidos: OptionItem[] = regcodes.map(({ code, name }) => ({
-        key: code.slice(0, 2),
-        text: name,
-      }));
-      allSidos.unshift({ key: "*", text: "모든 지역" });
-
-      setSelects((prev) => ({ ...prev, sidos: allSidos }));
-    }
-
-    async function getSidoguns(sidoCode: string) {
-      if (sidoCode === "*") {
-        setSelects((prev) => ({ ...prev, sigungus: [] }));
-        return;
-      }
-
-      const regcodes = await getRegSidoguns(sidoCode);
-
-      const newSidoguns: OptionItem[] = regcodes.map(({ code, name }) => ({
-        key: code.slice(2, 5),
-        text: name.split(" ").splice(1).join(" "),
-      }));
-      newSidoguns.unshift({ key: "*", text: "모든 지역" });
-      setSelects((prev) => ({ ...prev, sigungus: newSidoguns }));
-    }
-
-    getAllSidos();
-    getSidoguns(selected.sido);
-    console.log("sigungus:", selects.sigungus);
-  }, [selected.sido, selects.sidos, selects.sigungus]);
-
-  const createHandleChange = (key: keyof Selected) => {
-    return ({ target }: React.ChangeEvent<HTMLSelectElement>) => {
-      setSelected((prev) => ({ ...prev, [key]: target.value }));
-    };
+  const handleChangePet = ({ target }: HandleChangeSelect) => {
+    setPets((prev) => ({ ...prev, selected: target.value }));
   };
+  const handleChangeSido = ({ target }: HandleChangeSelect) => {
+    setSido((prev) => ({ ...prev, selected: target.value }));
+  };
+  const handleChangeSigungu = ({ target }: HandleChangeSelect) => {
+    setSigungu((prev) => ({ ...prev, selected: target.value }));
+  };
+  // const createHandleChange = (key: "pet" | "sido" | "sigungu") => {
+  //   return ({ target }: React.ChangeEvent<HTMLSelectElement>) => {
+  //     switch (key) {
+  //       case "pet":
+  //         return setPets((prev) => ({ ...prev, selected: target.value }));
+  //       case "sido":
+  //         return setSidos((prev) => ({ ...prev, selected: target.value }));
+  //       case "sigungu":
+  //         return setSigungus((prev) => ({ ...prev, selected: target.value }));
+  //       default:
+  //         throw new Error("Unhandled Key on createHandleChnage");
+  //     }
+  //   };
+  // };
 
   return (
     <form>
+      <Select optionItems={pet.items} handleChange={handleChangePet} />
+      <Select optionItems={sido.items} handleChange={handleChangeSido} />
       <Select
-        optionItems={selects.petKinds}
-        handleChange={createHandleChange("petKind")}
-      />
-      <Select
-        optionItems={selects.sidos}
-        handleChange={createHandleChange("sido")}
-      />
-      {selects.sigungus.length ? (
-        <Select
-          optionItems={selects.sigungus}
-          handleChange={createHandleChange("sigungu")}
-        />
-      ) : null}
+        optionItems={sigungu.items}
+        handleChange={handleChangeSigungu}
+      ></Select>
     </form>
   );
 }
