54a36cd24d405fe052854ab3d5b35bdee10342f7
unsnruu <unsnruu@gmail.com>
2022-04-23 13:27:19 +0900

    Add sumbit evnet handler

--- client/src/Pages/Write.tsx
+++ client/src/Pages/Write.tsx
@@ -1,32 +1,25 @@
 //Todo : 로그인 시에만 이 페이지 방문 가능하도록 navigate해야한다.
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import Select from "../components/Select";
+import Select, { OptionItem } from "../components/Select";
+
 import { useRegion } from "../api";
-import {
-  PetOption,
-  PetState,
-  HandleChangeSelect,
-} from "../components/Form/types";
-
-const petInitItems: PetOption[] = [
-  { key: "*", text: "모든 동물" },
-  { key: "dog", text: "강아지" },
-  { key: "cat", text: "고양이" },
-  { key: "misc", text: "기타" },
-];
+import { petInitItems } from "../common";
+import { PetState, HandleChangeSelect } from "../components/types";
 
 function Write() {
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const { sido, setSido, sigungu, setSigungu } = useRegion();
   const [pet, setPet] = useState<PetState>({
-    selected: "*",
+    selected: "null",
     items: petInitItems,
   });
-  const { sido, setSido, sigungu, setSigungu } = useRegion();
 
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const handleChangeTitle = ({
     target,
@@ -48,17 +41,56 @@ function Write() {
     setSigungu((prev) => ({ ...prev, selected: target.value }));
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log({
-      sido: sido.selected,
-      sigungu: sigungu.selected,
-      pet: pet.selected,
-      title,
-      text,
-    });
+
+    if (pet.selected === "null") {
+      window.alert("동물의 종류를 선택해 주세요");
+      return;
+    } else if (sido.selected === "*") {
+      window.alert("지역을 선택해 주세요");
+      return;
+    } else if (sigungu.selected === "*") {
+      window.alert("상세 지역을 선택해 주세요");
+      return;
+    } else if (!title.trim().length) {
+      window.alert("제목이 비었습니다.");
+      return;
+    } else if (!text.length) {
+      window.alert("내용이 비었습니다.");
+      return;
+    }
+
+    try {
+      await axios.post(
+        "/missing/write",
+        {
+          title,
+          text,
+          pet: pet.selected,
+          sido: sido.selected,
+          sigungu: sigungu.selected,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.log("Failed to submit");
+    }
     navigate("/missing", { replace: true });
   };
+
+  const petItems: OptionItem[] = [
+    { key: "null", text: "동물 선택", disabled: true },
+    ...pet.items,
+  ];
+  const sidoItems: OptionItem[] = [
+    { key: "*", text: "지역 선택", disabled: true },
+    ...sido.items,
+  ];
+  const sigunguItems: OptionItem[] = [
+    { key: "*", text: "상세 지역 선택", disabled: true },
+    ...sigungu.items,
+  ];
 
   return (
     <div id="page-write">
@@ -68,12 +100,14 @@ function Write() {
           <input type="text" onChange={handleChangeTitle} />
         </div>
         <div>
-          <Select optionItems={pet.items} handleChange={handleChangePet} />
-          <Select optionItems={sido.items} handleChange={handleChangeSido} />
-          <Select
-            optionItems={sigungu.items}
-            handleChange={handleChangeSigungu}
-          />
+          <Select optionItems={petItems} handleChange={handleChangePet} />
+          {<Select optionItems={sidoItems} handleChange={handleChangeSido} />}
+          {sido.selected === "*" || (
+            <Select
+              optionItems={sigunguItems}
+              handleChange={handleChangeSigungu}
+            />
+          )}
         </div>
         <textarea cols={30} rows={10} onChange={handleChangeText} />
         <button>작성 완료</button>
