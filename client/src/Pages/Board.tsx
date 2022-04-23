b42362b6a54882ac432a12dd0d6a7c19d6a517dd
unsnruu <unsnruu@gmail.com>
2022-04-23 13:26:32 +0900

    Add post fetch logic

--- client/src/Pages/Board.tsx
+++ client/src/Pages/Board.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
+import { useRegion } from "../api";
+import { PetState, HandleChangeSelect } from "../components/types";
+import { petInitItems } from "../common";
+
 import Post from "../components/Post";
-import { SearchForm } from "../components/Form";
+import Select from "../components/Select";
+import axios from "axios";
 
 type Pathname = "/missing" | "/care" | "/adoption" | string;
+
+interface Post {
+  id: string | number;
+  author: string;
+  title: string;
+  text: string;
+  sido?: string;
+  sigungu?: string;
+  createdAt?: Date;
+  pet?: string;
+}
+
+type AxiosPostData = Post[];
 
 function getTitle(pathname: Pathname) {
   let title = "";
@@ -26,7 +44,38 @@ function getTitle(pathname: Pathname) {
 
 function Board() {
   const { pathname } = useLocation();
+
+  const { sido, setSido, sigungu, setSigungu } = useRegion();
+  const [pet, setPet] = useState<PetState>({
+    selected: "*",
+    items: petInitItems,
+  });
+
+  const [posts, setPosts] = useState<Post[]>([]);
   let title = getTitle(pathname);
+
+  const handleChangePet = ({ target }: HandleChangeSelect) => {
+    setPet((prev) => ({ ...prev, selected: target.value }));
+  };
+  const handleChangeSido = ({ target }: HandleChangeSelect) => {
+    setSido((prev) => ({ ...prev, selected: target.value }));
+  };
+  const handleChangeSigungu = ({ target }: HandleChangeSelect) => {
+    setSigungu((prev) => ({ ...prev, selected: target.value }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("검색 실행");
+  };
+
+  useEffect(() => {
+    async function init() {
+      const { data } = await axios.post<AxiosPostData>("/missing/board/init");
+      setPosts((prev) => [...prev, ...data]);
+    }
+    init();
+  }, []);
 
   return (
     <div>
@@ -34,13 +83,23 @@ function Board() {
         <h2 style={{ display: "inline-block" }}>{title}</h2>
         <Link to={"write"}>게시물 작성하기</Link>
       </div>
-      <div id="search-form" style={{ display: "flex" }}>
-        <SearchForm />
+      <form id="search-form" onSubmit={handleSubmit}>
+        <Select optionItems={pet.items} handleChange={handleChangePet} />
+        <Select optionItems={sido.items} handleChange={handleChangeSido} />
+        {sido.selected === "*" || (
+          <Select
+            optionItems={sigungu.items}
+            handleChange={handleChangeSigungu}
+          />
+        )}
         <button>검색</button>
-      </div>
+      </form>
       <hr />
-      <main id="board-main">
-        <h3>BOARD</h3>
+      <h3>BOARD</h3>
+      <main id="board-main" style={{ display: "flex" }}>
+        {posts.map(({ author, id, title, text }) => (
+          <Post author={author} key={id} text={text} titile={title} />
+        ))}
       </main>
     </div>
   );
