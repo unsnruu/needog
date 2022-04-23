7fea23f8eadd51ef02bad9e8d36860f5d512c3a5
unsnruu <unsnruu@gmail.com>
2022-04-23 13:28:29 +0900

    Move type definition

--- client/src/Pages/Care.tsx
+++ client/src/Pages/Care.tsx
@@ -1,15 +1,5 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
-
-import Post from "../components/Post";
-import { SearchForm } from "../components/Form";
-
-interface PostType {
-  id: number;
-  author: string;
-  text: string;
-  titile: string;
-}
+import { Outlet } from "react-router-dom";
 
 function Care() {
   return <Outlet />;
