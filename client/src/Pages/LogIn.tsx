28eda71fe93010aa6c14f369fcd558409c89088f
unsnruu <unsnruu@gmail.com>
2022-04-27 01:59:18 +0900

    Add AxiosReturnType

--- client/src/Pages/LogIn.tsx
+++ client/src/Pages/LogIn.tsx
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 
-import authAtom from "../recoil/auth";
+import userAtom from "../recoil/user";
+
+interface AxiosReturnLogin {
+  email: string;
+  nickname: string;
+  userId: string;
+  snsId: string;
+}
 
 function LogIn() {
   const [form, setForm] = useState({ id: "", pwd: "" });
   const navigate = useNavigate();
-  const setAuth = useSetRecoilState(authAtom);
+  const setUser = useSetRecoilState(userAtom);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      const { data: user } = await axios.post("/auth/login", form, {
+      const { data } = await axios.post<AxiosReturnLogin>("/auth/login", form, {
         withCredentials: true,
       });
-      setAuth((prev) => ({ user, isLoggedIn: true }));
-      console.log("성공적으로 로그인하셨습니다.");
+      const { email, nickname, snsId, userId } = data;
+      console.log(data);
+      setUser((prev) => ({ ...prev, nickname, userId, snsId }));
     } catch (err) {
       console.log("로그인에 실패하셨습니다.");
     } finally {
