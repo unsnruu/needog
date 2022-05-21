3e2f71b9ec911baf2cb49399b214d686549cb24f
unsnruu <unsnruu@gmail.com>
2022-05-21 12:45:08 +0900

    Create apis/index.ts for controlling apis together

---
+++ client/src/apis/index.ts
@@ -0,0 +1,15 @@
+import getRegion from "./getRegion";
+import useRegion from "./useRegion";
+import useSearch from "./useSearch";
+import deepCloneObject from "./deepCloneObject";
+import { getBasePathname } from "./getBasePathname";
+import { getSigunguQuery } from "./getSigunguQuery";
+
+export {
+  getRegion,
+  useRegion,
+  useSearch,
+  deepCloneObject,
+  getBasePathname,
+  getSigunguQuery,
+};
