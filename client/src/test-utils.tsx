357fbe76f5a73f87cc8f1d5e48aafcccf106ea1a
unsnruu <unsnruu@gmail.com>
2022-05-21 12:45:43 +0900

    Remove MemoryRouter

--- client/src/test-utils.tsx
+++ client/src/test-utils.tsx
@@ -1,14 +1,9 @@
 import React, { ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
-import { MemoryRouter } from "react-router-dom";
 
 const AllTheBoardProvider: React.FC = ({ children }) => {
-  return (
-    <RecoilRoot>
-      <MemoryRouter initialEntries={["/missing"]}>{children}</MemoryRouter>
-    </RecoilRoot>
-  );
+  return <RecoilRoot>{children}</RecoilRoot>;
 };
 
 const boardCustomRender = (
