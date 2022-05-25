8237b93870fdb6ae54d79a3f4ba7851062382059
unsnruu <unsnruu@gmail.com>
2022-05-26 02:35:10 +0900

    Test sucessfully initializing sigunguAtom

--- client/src/recoil/sigungu/atom.ts
+++ client/src/recoil/sigungu/atom.ts
@@ -1,32 +1,42 @@
 import { atom, AtomEffect } from "recoil";
+import sidoAtom from "../sido";
 import { OptionItem } from "../../common/types";
 
-const initialSigungu = {
-  "1100000000": [],
-  "2600000000": [],
-  "2800000000": [],
-  "2900000000": [],
-  "3000000000": [],
-  "3100000000": [],
-  "4100000000": [],
-  "4200000000": [],
-  "4300000000": [],
-  "4400000000": [],
-  "4500000000": [],
-  "4600000000": [],
-  "4700000000": [],
-  "4800000000": [],
-  "5000000000": [],
-};
+// const initialSigungu = {
+//   "1100000000": [],
+//   "2600000000": [],
+//   "2800000000": [],
+//   "2900000000": [],
+//   "3000000000": [],
+//   "3100000000": [],
+//   "4100000000": [],
+//   "4200000000": [],
+//   "4300000000": [],
+//   "4400000000": [],
+//   "4500000000": [],
+//   "4600000000": [],
+//   "4700000000": [],
+//   "4800000000": [],
+//   "5000000000": [],
+// };
 
 type SigunguAtom = { [key: string]: OptionItem[] };
-const initSigunguAtom: AtomEffect<SigunguAtom> = ({ node, setSelf }) => {
-  setSelf({ a: [] });
+const getSigunguFromJuso = async (sidoCode: string) => {};
+
+const initSigunguAtom: AtomEffect<SigunguAtom> = ({ getLoadable, setSelf }) => {
+  const sidoAtomValue = getLoadable(sidoAtom).getValue();
+  const sidoCodes = sidoAtomValue.map(({ key }) => key);
+
+  const newSigungu: SigunguAtom = {};
+  sidoCodes.forEach((code) => {
+    if (!newSigungu[code]) newSigungu[code] = [];
+  });
+  setSelf(newSigungu);
 };
 
-const sigunguAtom = atom({
+const sigunguAtom = atom<SigunguAtom>({
   key: "sigunguAtom",
-  default: initialSigungu,
+  default: {},
   effects: [initSigunguAtom],
 });
 
--- client/src/recoil/sigungu/sigunguAtom.test.ts
+++ client/src/recoil/sigungu/sigunguAtom.test.ts
@@ -1,11 +1,27 @@
 import { snapshot_UNSTABLE } from "recoil";
 import { sigunguAtom } from "./atom";
 
+const initialSigungu = {
+  "1100000000": [],
+  "2600000000": [],
+  "2800000000": [],
+  "2900000000": [],
+  "3000000000": [],
+  "3100000000": [],
+  "4100000000": [],
+  "4200000000": [],
+  "4300000000": [],
+  "4400000000": [],
+  "4500000000": [],
+  "4600000000": [],
+  "4700000000": [],
+  "4800000000": [],
+  "5000000000": [],
+};
+
 test("recoil이 제대로 작동되는지 확인", () => {
   const initialSnapshot = snapshot_UNSTABLE();
   const value = initialSnapshot.getLoadable(sigunguAtom).getValue();
-  console.log(value);
-  expect(value).toMatchObject({
-    a: [],
-  });
+
+  expect(value).toMatchObject(initialSigungu);
 });
