cf4e0ae4d985add09a945caee00a2afdfe640333
unsnruu <unsnruu@gmail.com>
2022-05-27 08:22:51 +0900

    Remove event handler for restructuring

--- client/src/components/Select.tsx
+++ client/src/components/Select.tsx
@@ -3,19 +3,14 @@ import { OptionItem } from "../common/types";
 
 type SelectProps = {
   name: string;
-  optionItems: OptionItem[];
-  handleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  optionItems: OptionItem[] | null;
 };
 
-function Select({ name, optionItems, handleChange }: SelectProps) {
-  if (!optionItems.length) return null;
+function Select({ name, optionItems }: SelectProps) {
+  if (!optionItems) return null;
 
   return (
-    <select
-      onChange={handleChange}
-      defaultValue={optionItems[0].key}
-      data-testid={name}
-    >
+    <select defaultValue={optionItems[0].key} data-testid={name}>
       {optionItems.map(({ key, text, disabled }) => (
         <option key={key} value={key} label={text}>
           {text}
