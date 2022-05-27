a2a2518928b2020a754d4a7af93af2a49bdc37c5
unsnruu <unsnruu@gmail.com>
2022-05-27 13:14:10 +0900

    Add getTitleByPathname function

--- client/src/components/Board/BoardHeading.tsx
+++ client/src/components/Board/BoardHeading.tsx
@@ -3,18 +3,28 @@ import { Link } from "react-router-dom";
 import { Grid, Typography } from "@mui/material";
 
 interface BoardHeadingProps {
-  title: string;
-  isLoggedIn: boolean;
+  pathname: string;
 }
-export default function BoardHeading({ title, isLoggedIn }: BoardHeadingProps) {
+export default function BoardHeading({ pathname }: BoardHeadingProps) {
+  const title = getTitleByPathname(pathname);
   return (
     <Grid item xs={12} container sx={{ marginBottom: "2rem" }}>
       <Grid item xs={12} container justifyContent={"center"}>
         <Typography sx={{ typography: "h3" }}>{title}</Typography>
       </Grid>
-      <Grid item xs={12}>
-        {isLoggedIn && <Link to={"/missing/write"}>글쓰기</Link>}
-      </Grid>
     </Grid>
   );
+}
+
+function getTitleByPathname(pathname: string): string {
+  switch (pathname) {
+    case "/missing":
+      return "실종";
+    case "/care":
+      return "보호";
+    case "adoption":
+      return "입양";
+    default:
+      return "알 수 없는 주소";
+  }
 }
