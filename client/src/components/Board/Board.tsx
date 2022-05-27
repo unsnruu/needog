c5884b3d7d94cc9bdd24f7a468363ea471f9b0e4
unsnruu <unsnruu@gmail.com>
2022-05-27 13:26:33 +0900

    Add useEffect to fetch cardItems

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -1,22 +1,23 @@
-// 보드 자체만 테스트를 하는데 4번의 렌더링이 추가적으로 발생하는 것을 보면
-// 보드 내부 혹은 useRegion 내부의 문제가 아닐까 싶다.ㅡ
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 import BoardHeading from "./BoardHeading";
 import SearchForm from "../SearchForm";
 import BoardMain from "./BoardMain";
 import { Grid, SelectChangeEvent } from "@mui/material";
 
-import { Selected, SearchKey } from "../../common/types";
+import { Selected, CardItem } from "../../common/types";
 
 function Board() {
+  const { pathname } = useLocation();
   //<SearchFrom>의 changeEvnet에 따라 변경되는 값을 추적하기 위해 local state 사용
   const [selected, setSelected] = useState<Selected>({
     pet: "",
     sido: "",
     sigungu: "",
   });
-
+  //sigungu가 sido에 의존적이기 때문에 이벤트 함수를 일일이 만들었음.
   const handleChangePet = (event: SelectChangeEvent) => {
     setSelected((prev) => ({ ...prev, pet: event.target.value }));
   };
@@ -26,16 +27,27 @@ function Board() {
   const handleChangeSigungu = (event: SelectChangeEvent) => {
     setSelected((prev) => ({ ...prev, sigungu: event.target.value }));
   };
+
+  const [cardItems, setCardItems] = useState<CardItem[]>([]);
+  //carditems를 가져오는 effect hook
+  useEffect(() => {
+    async function fetchCardItem() {
+      const { data } = await axios.post<CardItem[]>(`${pathname}/board/init`);
+      setCardItems(data);
+    }
+    fetchCardItem();
+  }, [pathname]);
+
   return (
     <Grid container justifyContent={"center"}>
-      {/* <BoardHeading /> */}
+      <BoardHeading pathname={pathname} />
       <SearchForm
         selected={selected}
         handleChangePet={handleChangePet}
         handleChangeSido={handleChangeSido}
         handleChangeSigungu={handleChangeSigungu}
       />
-      {/* <BoardMain /> */}
+      <BoardMain cardItems={cardItems} pathname={pathname} />
     </Grid>
   );
 }
