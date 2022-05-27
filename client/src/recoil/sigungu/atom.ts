2cef9902583d99cbdda7e86f12f1bd593bb62549
unsnruu <unsnruu@gmail.com>
2022-05-27 12:45:49 +0900

    Remove fetch effect

--- client/src/recoil/sigungu/atom.ts
+++ client/src/recoil/sigungu/atom.ts
@@ -1,7 +1,9 @@
 import { atom, AtomEffect } from "recoil";
 import sidoAtom from "../sido";
 import { OptionItem } from "../../common/types";
-import { getRegion, getSigunguQuery } from "../../apis";
+import { getRegion } from "../../apis";
+
+//! fetchSigunguEffect가 SearchForm에서 약 30번의 리렌더링을 발생시킨다.
 
 type SigunguAtom = { [key: string]: OptionItem[] };
 
@@ -11,50 +13,59 @@ const initSigunguEffect: AtomEffect<SigunguAtom> = ({
 }) => {
   const sidoAtomValue = getLoadable(sidoAtom).getValue();
   const sidoCodes = sidoAtomValue.map(({ key }) => key);
-
   const newSigungu: SigunguAtom = {};
+
   sidoCodes.forEach((code) => {
     if (!newSigungu[code]) newSigungu[code] = [];
   });
-
   setSelf(newSigungu);
-};
-const trimRawItems = ({ key, text }: OptionItem) => ({
-  key,
-  text: text.split(" ").slice(1).join(" "),
-});
-const fetchSigunguEffect: AtomEffect<SigunguAtom> = ({
-  node,
-  getLoadable,
-  setSelf,
-  trigger,
-}) => {
-  const getSigunguValueFromJuso = async (sidoCode: string) => {
-    const sigunguRawOptionItems = await getRegion(getSigunguQuery(sidoCode));
-
-    if (!sigunguRawOptionItems) return;
-    const sigunguOptionItems = sigunguRawOptionItems.map((item) =>
-      trimRawItems(item)
-    );
-    const newState = {
-      ...getLoadable(node).getValue(),
-      [sidoCode]: sigunguOptionItems,
-    };
-
-    setSelf(newState);
-  };
-
-  if (trigger === "get") {
-    const sigunguValue = getLoadable(node).getValue();
-    const keys = Object.keys(sigunguValue);
-    keys.forEach(async (key) => getSigunguValueFromJuso(key));
-  }
 };
 
 const sigunguAtom = atom<SigunguAtom>({
   key: "sigunguAtom",
   default: {},
-  effects: [initSigunguEffect, fetchSigunguEffect],
+  effects: [initSigunguEffect],
 });
 
 export { sigunguAtom };
+
+// const trimRawItems = ({ key, text }: OptionItem) => ({
+//   key,
+//   text: text.split(" ").slice(1).join(" "),
+// });
+
+// const fetchSigunguEffect: AtomEffect<SigunguAtom> = ({
+//   node,
+//   getLoadable,
+//   setSelf,
+//   trigger,
+//   onSet,
+// }) => {
+//   const getSigunguValueFromJuso = async () => {
+//     const allSigunguOptionItem = await getRegion("*00000", true);
+
+//     if (!allSigunguOptionItem) return;
+//     const newSigungu: SigunguAtom = {};
+//     allSigunguOptionItem.forEach((item) => {
+//       const { key } = item;
+//       const newItemKey = key.slice(0, 2) + "00000000";
+//       if (!newSigungu[newItemKey]) newSigungu[newItemKey] = [];
+//       newSigungu[newItemKey].push(item);
+//     });
+//     // if (!sigunguRawOptionItems) return;
+//     // const sigunguOptionItems = sigunguRawOptionItems.map((item) =>
+//     //   trimRawItems(item)
+//     // );
+//     setSelf(newSigungu);
+//   };
+
+//   if (trigger === "get") {
+//     const sigunguValue = getLoadable(node).getValue();
+//     const keys = Object.keys(sigunguValue);
+//     keys.forEach(async (key) => getSigunguValueFromJuso());
+//   }
+
+//   onSet((newVal, preVal, isReset) => {
+//     console.log("isReset", isReset);
+//   });
+// };
