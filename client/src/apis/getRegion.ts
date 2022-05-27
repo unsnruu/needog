f43a188038629851cd29e2c02c8cc9dbeeee9e62
unsnruu <unsnruu@gmail.com>
2022-05-27 15:58:17 +0900

    Add isIgnore flage on params

--- client/src/apis/getRegion.ts
+++ client/src/apis/getRegion.ts
@@ -1,19 +1,23 @@
+// https://juso.dev/
+
 import axios from "axios";
 import { OptionItem, RegCode, RegCodes } from "../common/types";
 
-const API_URL = `https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes`;
+const API_URL = `https://grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=`;
 const convertToOptionItem = ({ code, name }: RegCode): OptionItem => ({
   key: code,
   text: name,
 });
 
-export default async function getRegion(query: string | null) {
-  if (!query) return;
+export default async function getRegion(query: string, isIgnore: boolean) {
+  const ignoreFlag = isIgnore ? "&is_ignore_zero=true" : "";
+  const requestURL = API_URL + query + ignoreFlag;
 
-  const requestURL = API_URL + "?regcode_pattern=" + query;
   try {
-    const { data } = await axios.get<RegCodes>(requestURL);
-    const { regcodes } = data;
+    const {
+      data: { regcodes },
+    } = await axios.get<RegCodes>(requestURL);
+
     if (!regcodes.length) return;
 
     //regCodes를 OptionItem형식으로 바꾸기
