f2e60aa173e996febcb874e40cec8897245fff12
unsnruu <unsnruu@gmail.com>
2022-05-30 10:40:16 +0900

    Add icons

--- client/src/components/Tiptap.tsx
+++ client/src/components/Tiptap.tsx
@@ -1,23 +1,68 @@
-import { useEditor, EditorContent } from "@tiptap/react";
+import { useState } from "react";
+import { useEditor, EditorContent, Editor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Image from "@tiptap/extension-image";
+import axios from "axios";
+
 import styled from "@emotion/styled";
 
 import { Grid } from "@mui/material";
+import {
+  FormatBold,
+  FormatItalic,
+  FormatUnderlined,
+  StrikethroughS,
+  FormatSize,
+  FormatListBulleted,
+  FormatAlignCenter,
+  FormatAlignLeft,
+  FormatAlignRight,
+  AddAPhoto,
+} from "@mui/icons-material";
 
 const StyledEditorContent = styled(EditorContent)`
   outline: 1px solid black;
 `;
 
 export default function Tiptap() {
+  const [file, setFile] = useState<File | null>(null);
+
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [StarterKit, Image],
     content: "<h1>Hello World!</h1>",
   });
 
   return (
     <Grid>
+      <TiptapMenubar editor={editor} />
       <StyledEditorContent editor={editor} />
+      <AddAPhoto
+        onClick={() => {
+          console.log("clicked");
+        }}
+      />
     </Grid>
+  );
+}
+
+interface TiptapMenubarProps {
+  editor: Editor | null;
+}
+function TiptapMenubar({ editor }: TiptapMenubarProps) {
+  if (!editor) return null;
+
+  return (
+    <>
+      <FormatBold />
+      <FormatItalic />
+      <FormatUnderlined />
+      <StrikethroughS />
+      <FormatSize />
+      <FormatListBulleted />
+      <FormatAlignLeft />
+      <FormatAlignCenter />
+      <FormatAlignRight />
+    </>
   );
 }
 
