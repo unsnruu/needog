bb5c69d8e5a3416334c3596097638c9049f62d34
unsnruu <unsnruu@gmail.com>
2022-06-02 00:03:51 +0900

    Add event handlers

--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -19,23 +19,25 @@ import {
 } from "@mui/material";
 
 interface UserInfo {
-  userId: string;
+  username: string;
   password: string;
-  rePassword: string;
+  passwordConfirm: string;
   nickname: string;
 }
 function SignUp() {
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState<UserInfo>({
-    userId: "",
+    username: "",
     password: "",
-    rePassword: "",
+    passwordConfirm: "",
     nickname: "",
   });
   const [open, setOpen] = useState(false);
   const [error, setError] = useState("");
 
   const isEmptyString = (value: string) => (value === "" ? true : false);
+  const isFilledIn = (userInfoKey: keyof UserInfo) =>
+    userInfo[userInfoKey] === "" ? false : true;
   const isAllFilledIn = (userInfo: UserInfo): boolean => {
     const keys = Object.keys(userInfo) as Array<keyof UserInfo>;
     for (let key of keys) {
@@ -44,7 +46,7 @@ function SignUp() {
     return true;
   };
   const isSamePassword = () => {
-    if (userInfo.password === userInfo.rePassword) return true;
+    if (userInfo.password === userInfo.passwordConfirm) return true;
     else return false;
   };
 
@@ -56,41 +58,48 @@ function SignUp() {
         let message = "모든 필드가 채워지지 않았습니다.";
         setError(message);
         setOpen(true);
-        throw new Error("모든 필드가 채워지지 않았습니다.");
+        throw new Error(message);
       }
       if (!isSamePassword()) {
         let message = "비밀번호를 다시 확인해 주세요";
-        setError("비밀번호를 다시 확인해 주세요");
+        setError(message);
         setOpen(true);
         throw new Error(message);
       }
-      await axios.post("/auth/signup", { ...userInfo });
+      const result = await axios.post("/auth/signup", { ...userInfo });
+      console.log(result);
       navigate("/", { replace: true });
     } catch (error) {
+      console.log(error);
       console.error("계정 만들기에 실패함");
     }
   };
-
+  //event handlers
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setUserInfo((prev) => {
       return { ...prev, [target.id]: target.value };
     });
   };
+  const handleBlur = ({ target }: React.FocusEvent<HTMLInputElement>) => {
+    if (!isFilledIn(target.id as keyof UserInfo)) {
+      //todo 다 채워지지 않았다면 어떤 <상태를 이용>해서 form field에 뜨도록
+    }
+  };
+
   const handleClose = (
     event: React.SyntheticEvent | Event,
     reason?: string
   ) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setError("");
+    if (reason === "clickaway") return;
     setOpen(false);
+    setError("");
+    //todo: setError가 동시에 되면서 ui가 어그러지는 현상 발생. setTimeout 사용을 고려해 볼 것
   };
   return (
     <>
       <Grid container justifyContent={"center"} aria-label="양식">
         <SingUpHeader />
-        <SignUpForm handleChange={handleChange} />
+        <SignUpForm handleChange={handleChange} handleBlur={handleBlur} />
         <Grid item xs={10} container>
           <Button variant="contained" onClick={handleClickSubmit} fullWidth>
             가입하기
@@ -108,14 +117,6 @@ function SignUp() {
 
 export { SignUp };
 
-function SingUpHeader() {
-  return (
-    <Grid item xs={12} md={12} container justifyContent={"center"}>
-      <Typography>회원가입</Typography>
-    </Grid>
-  );
-}
-
 type TextFieldItem = {
   id: string;
   label: string;
@@ -123,17 +124,19 @@ type TextFieldItem = {
 };
 interface SigunUpFormProps {
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
-function SignUpForm({ handleChange }: SigunUpFormProps) {
+function SignUpForm({ handleChange, handleBlur }: SigunUpFormProps) {
   const textfieldItems: TextFieldItem[] = [
-    { id: "userId", label: "아이디", type: "text" },
+    { id: "username", label: "아이디", type: "text" },
     { id: "nickname", label: "닉네임", type: "text" },
     { id: "password", label: "비밀번호", type: "password" },
-    { id: "rePassword", label: "비밀번호 확인", type: "password" },
+    { id: "passwordConfirm", label: "비밀번호 확인", type: "password" },
   ];
   const textFieldStyle: SxProps<Theme> = {
     marginBottom: "1rem",
   };
+
   return (
     <Grid item container justifyContent={"center"}>
       {textfieldItems.map(({ id, label, type }) => (
@@ -145,9 +148,17 @@ function SignUpForm({ handleChange }: SigunUpFormProps) {
             onChange={handleChange}
             fullWidth
             required
+            onBlur={handleBlur}
           />
         </Grid>
       ))}
+    </Grid>
+  );
+}
+function SingUpHeader() {
+  return (
+    <Grid item xs={12} md={12} container justifyContent={"center"}>
+      <Typography>회원가입</Typography>
     </Grid>
   );
 }
