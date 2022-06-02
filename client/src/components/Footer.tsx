7fb0e2f24b60522882cb38bbafd2f2d2181e1e71
unsnruu <unsnruu@gmail.com>
2022-06-02 16:49:29 +0900

    Add styles

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { Grid } from "@mui/material";
+
 function Footer() {
-  return <footer>@Footer</footer>;
+  return (
+    <Grid
+      container
+      justifyContent={"center"}
+      alignItems={"center"}
+      sx={{ height: "5rem" }}
+    >
+      @Footer
+    </Grid>
+  );
 }
 
 export default Footer;
