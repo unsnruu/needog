3627caf4e5f52a58e649a87f998ef2d7a3ba0dc7
unsnruu <unsnruu@gmail.com>
2022-06-03 01:19:32 +0900

    Move files

--- client/src/common/data.ts
+++
@@ -1,9 +0,0 @@
-import { OptionItem } from "./types";
-
-const petInitItems: OptionItem[] = [
-  { key: "dog", text: "강아지" },
-  { key: "cat", text: "고양이" },
-  { key: "misc", text: "기타" },
-];
-
-export { petInitItems };
