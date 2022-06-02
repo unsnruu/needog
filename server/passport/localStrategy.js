f5b40e2a9d6cd583a1a5fd97084ffd4f176e9d08
unsnruu <unsnruu@gmail.com>
2022-06-02 19:09:59 +0900

    Rename parameters and Remove options

--- server/passport/localStrategy.js
+++ server/passport/localStrategy.js
@@ -3,28 +3,26 @@ const LocalStrategy = require("passport-local");
 const bycrypt = require("bcrypt");
 const { findUser } = require("../database/sql");
 
-const strategy = new LocalStrategy(
-  { usernameField: "id", passwordField: "pwd" },
-  async (id, password, done) => {
-    console.log("LocalStrategy has executed");
-    try {
-      const user = await findUser({ userId: id });
-      if (user) {
-        const result = await bycrypt.compare(password, user.password);
-        if (result) {
-          done(null, user);
-        } else {
-          done(null, false, { message: "비밀번호가 일치하지 않습니다." });
-        }
+const strategy = new LocalStrategy(async (username, password, done) => {
+  console.log("LocalStrategy has executed");
+  console.log(username, password);
+  try {
+    const user = await findUser({ userId: username });
+    if (user) {
+      const result = await bycrypt.compare(password, user.password);
+      if (result) {
+        done(null, user);
       } else {
-        done(null, false, { message: "가입되지 않은 회원입니다." });
+        done(null, false, { message: "비밀번호가 일치하지 않습니다." });
       }
-    } catch (error) {
-      console.error(error);
-      done(error);
+    } else {
+      done(null, false, { message: "가입되지 않은 회원입니다." });
     }
+  } catch (error) {
+    console.error(error);
+    done(error);
   }
-);
+});
 
 module.exports = () => {
   passport.use(strategy);
